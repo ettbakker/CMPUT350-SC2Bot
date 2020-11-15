c28a0f4262e100f58eda3cbc9d623944475fb6cf
dsunderm <dsunderm@ualberta.ca>
2020-11-15 14:11:17 -0700

    Added functions and organized

--- BasicSc2Bot.h
+++ BasicSc2Bot.h
@@ -16,91 +16,30 @@ public:
 	virtual void OnGameStart();
 	virtual void OnStep();
 	virtual void OnUnitIdle(const Unit* unit);
+	virtual void OnGameOver();
 
 private:
-	size_t CountUnitType(UNIT_TYPEID unit_type) 
-	{
-		return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
-	}
+	//Multiple-use Functions
+	size_t CountUnitType(UNIT_TYPEID unit_type);
+	bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+	bool TryBuildStructureAtPoint(ABILITY_ID ability_type_for_structure, Point2D point, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+	const Unit* FindNearestUnit(const Point2D& start, UNIT_TYPEID unit_type);
 
-	bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV)
-	{
-		const ObservationInterface* observation = Observation();
-		// If a unit already is building a supply structure of this type, do nothing.
-		// Also get an scv to build the structure.
-		const Unit* unit_to_build = nullptr;
-		Units units = observation->GetUnits(Unit::Alliance::Self);
-		for (const auto& unit : units)
-		{
-			for (const auto& order : unit->orders)
-			{
-				if (order.ability_id == ability_type_for_structure)
-				{
-					return false;
-				}
-			}
-			if (unit->unit_type == unit_type) 
-			{
-				unit_to_build = unit;
-			}
-		}
-		float rx = GetRandomScalar();
-		float ry = GetRandomScalar();
+	//Build Buildings
+	bool TryBuildSupplyDepot();
+	bool TryBuildRefinery();
+	bool TryBuildCommandCenter();
+	bool TryBuildBarracks();
+	bool TryBuildEngineeringBay();
+	bool TryBuildTurrets();
 
-		Actions()->UnitCommand(unit_to_build,
-			ability_type_for_structure,
-			Point2D(unit_to_build->pos.x + rx * 15.0f, unit_to_build->pos.y + ry * 15.0f));
+	//Attacking
+	bool AttackEnemy();
+	bool fixBuildings();
 
-		return true;
-	}
-
-	bool TryBuildSupplyDepot()
-	{
-		const ObservationInterface* observation = Observation();
-		// If we are not supply capped, don't build a supply depot.
-		if (observation->GetFoodUsed() <= observation->GetFoodCap() - 2)
-			return false;
-
-		// Try and build a depot. Find a random SCV and give it the order.
-		return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
-	}
-
-	const Unit* FindNearestMineralPatch(const Point2D& start) 
-	{
-	Units units = Observation()->GetUnits(Unit::Alliance::Neutral);
-	float distance = std::numeric_limits<float>::max();
-	const Unit * target = nullptr;
-	for (const auto& u : units) 
-	{
-		if (u->unit_type == UNIT_TYPEID::NEUTRAL_MINERALFIELD) 
-		{
-			float d = DistanceSquared2D(u->pos, start);
-			if (d < distance) 
-			{
-				distance = d;
-				target = u;		
-			}			
-		}		
-	}
-
-	return target;	
-	}
-
-	bool TryBuildBarracks() {
-	const ObservationInterface * observation = Observation();
-
-	if (CountUnitType(UNIT_TYPEID::TERRAN_SUPPLYDEPOT) < 1) 
-	{
-		return false;
-	}
-	
-	if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) > 0) 
-	{
-		return false;
-	}
-
-	return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS);
-	}
+	//Variables
+	size_t randomMarineLocation = 0;
+	bool enemySpotted = false;
 };
 
 #endif
