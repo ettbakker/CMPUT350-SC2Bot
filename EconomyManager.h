dc80246724e2149c41310ec65cdcf2375d6cc9d4
jtbakker <jtbakker@ualberta.ca>
2020-12-02 18:02:46 -0700

    Manager for checking costs

---
+++ EconomyManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Manager.h"
+#include "TerranUnitCosts.h"
+
+class EconomyManager : public Manager {
+	EconomyManager() { };
+
+	void UpdateResourceCounts();
+
+	bool CanAffordBuilding(UNIT_TYPEID building_type);
+	bool CanAffordUnit(UNIT_TYPEID unit_type);
+	bool CanAffordUpgrade(UPGRADE_ID upgrade_type);
+
+	int minerals=0, gas=0, food_used=0, food_cap=0;
+};
