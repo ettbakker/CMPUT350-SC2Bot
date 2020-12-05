f5b2c6e5aade9c8cfbb7af7214f54d510c0e9c3a
JandaileD <43227764+JandaileD@users.noreply.github.com>
2020-12-04 17:24:12 -0700

    Hellion and Siege tank methods
    Added morphing for both units. Should morph when just reaching the enemy and morph back when retreating.
    Did not test siege tanks yet so functionality may not be right.

--- CombatManager.cpp
+++ CombatManager.cpp
@@ -36,6 +36,8 @@ bool CombatManager::AttackEnemy() {
 	Units tank = observation->GetUnits(Unit::Alliance::Self, filter);
 	filter = IsUnit(UNIT_TYPEID::TERRAN_HELLION);
 	Units hellion = observation->GetUnits(Unit::Alliance::Self, filter);
+	filter = IsUnit(UNIT_TYPEID::TERRAN_HELLIONTANK);
+	Units hellbat = observation->GetUnits(Unit::Alliance::Self, filter);
 
 	if (marines.size() > 1) {
 		if (attack && (target != nullptr)) {
@@ -45,7 +47,7 @@ bool CombatManager::AttackEnemy() {
 			actions->UnitCommand(reapers, ABILITY_ID::ATTACK_ATTACK, target);
 			actions->UnitCommand(marauder, ABILITY_ID::ATTACK_ATTACK, target);
 			actions->UnitCommand(tank, ABILITY_ID::ATTACK_ATTACK, target);
-			actions->UnitCommand(hellion, ABILITY_ID::ATTACK_ATTACK, target);
+			HellionMorph(hellion, attack, target->pos);
 			return true;
 		} else if ((observation->GetArmyCount() > 80) && (target != nullptr)) {
 			//TODO: Allow other units to attack
@@ -54,7 +56,7 @@ bool CombatManager::AttackEnemy() {
 			actions->UnitCommand(reapers, ABILITY_ID::ATTACK_ATTACK, target);
 			actions->UnitCommand(marauder, ABILITY_ID::ATTACK_ATTACK, target);
 			actions->UnitCommand(tank, ABILITY_ID::ATTACK_ATTACK, target);
-			actions->UnitCommand(hellion, ABILITY_ID::ATTACK_ATTACK, target);
+			HellionMorph(hellion, attack, target->pos);
 			return true;
 		}
 		else if (observation->GetArmyCount() > 80) { 
@@ -74,7 +76,16 @@ bool CombatManager::AttackEnemy() {
 			//actions->UnitCommand(marauder, ABILITY_ID::ATTACK, attackPoint,true);
 			//actions->UnitCommand(tank, ABILITY_ID::ATTACK, attackPoint,true);
 			if (CountUnitType(UNIT_TYPEID::TERRAN_HELLION) > 0) {
+				//If almost to enemy, transform
+				float d = DistanceSquared2D(hellion[0]->pos, attackPoint);
+				if (d < 30) {
+					for (const auto& h : hellion) {
+						actions->UnitCommand(h, ABILITY_ID::MORPH_HELLBAT);
+						actions->UnitCommand(h, ABILITY_ID::ATTACK, attackPoint, true);
+					}
+				}
 				actions->UnitCommand(hellion[0], ABILITY_ID::ATTACK, attackPoint, true);
+				
 			}
 			else {
 				actions->UnitCommand(marines[0], ABILITY_ID::ATTACK, attackPoint,true);
@@ -95,11 +106,9 @@ bool CombatManager::AttackEnemy() {
 			actions->UnitCommand(reapers, ABILITY_ID::SMART, recallPoint);
 			actions->UnitCommand(marauder, ABILITY_ID::SMART, recallPoint);
 			actions->UnitCommand(tank, ABILITY_ID::SMART, recallPoint);
-			actions->UnitCommand(hellion, ABILITY_ID::SMART, recallPoint);
+			HellionMorph(hellbat, false, recallPoint);
 		}
 	}
-	
-
 
 	return false;
 }
@@ -111,6 +120,8 @@ void CombatManager::OnIdleMarine(const Unit* unit) {
 	size_t numberMarines = CountUnitType(UNIT_TYPEID::TERRAN_MARINE);
 	float rx = GetRandomScalar();
 	float ry = GetRandomScalar();
+
+	actions->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, halfway);
 	//Send every #th idle marine to a random enemy location so that we can spot enemy
 	if ((numberIdleMarines % 70) == 0) {
 		randomMarineLocation = rand() % expansionLocations.size();
@@ -128,5 +139,64 @@ void CombatManager::OnIdleMarine(const Unit* unit) {
 }
 
 void CombatManager::OnIdleReaper(const Unit* unit) {
+	actions->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, halfway);
+}
 
+void CombatManager::OnIdleMarauder(const Unit* unit) {
+	actions->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, halfway);
+}
+
+void CombatManager::OnIdleHellion(const Unit* unit) {
+	if (unit->unit_type == UNIT_TYPEID::TERRAN_HELLIONTANK && CountUnitType(UNIT_TYPEID::TERRAN_ARMORY) != 0) {
+		actions->UnitCommand(unit, ABILITY_ID::MORPH_HELLION);
+	}
+	actions->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, halfway);
+}
+
+void CombatManager::OnIdleSeigeTank(const Unit* unit) {
+	if (unit->unit_type == UNIT_TYPEID::TERRAN_SIEGETANKSIEGED) {
+		actions->UnitCommand(unit, ABILITY_ID::MORPH_UNSIEGE);
+	}
+	actions->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, halfway);
+}
+
+//Ultilities for morphing units
+void CombatManager::HellionMorph(const Units units, bool attacking, Point2D to) {
+	if (!units.empty()) {
+		float distanceAway = Distance2D(units.back()->pos, to);
+		for (const auto& hellion : units) {
+			// if attacking, transform to Hellbat and attack target
+			if (attacking) {
+				if (distanceAway < 30 && (CountUnitType(UNIT_TYPEID::TERRAN_ARMORY) != 0)) {
+					actions->UnitCommand(hellion, ABILITY_ID::MORPH_HELLBAT);
+				}
+				actions->UnitCommand(hellion, ABILITY_ID::ATTACK_ATTACK, to);
+			}
+			// if retreating, transform to Hellion and move to location
+			else {
+				actions->UnitCommand(hellion, ABILITY_ID::MORPH_HELLION);
+				actions->UnitCommand(hellion, ABILITY_ID::SMART, to);
+			}
+		}
+	}
+}
+
+void CombatManager::SiegeTankMorph(const Units units, bool attacking, Point2D to) {
+	if (!units.empty()) {
+		float distanceAway = Distance2D(units.front()->pos, to);
+		for (const auto& siegeTank : units) {
+			// if attacking, transform to Hellbat and attack target
+			if (attacking && (CountUnitType(UNIT_TYPEID::TERRAN_ARMORY) != 0)) {
+				if (distanceAway < 30) {
+					actions->UnitCommand(siegeTank, ABILITY_ID::MORPH_SIEGEMODE);
+				}
+				actions->UnitCommand(siegeTank, ABILITY_ID::ATTACK_ATTACK, to);
+			}
+			// if retreating, transform to Hellion and move to location
+			else {
+				actions->UnitCommand(siegeTank, ABILITY_ID::MORPH_UNSIEGE);
+				actions->UnitCommand(siegeTank, ABILITY_ID::SMART, to);
+			}
+		}
+	}
 }
--- CombatManager.h
+++ CombatManager.h
@@ -12,9 +12,21 @@ public:
 	CombatManager();
 
 	bool AttackEnemy();
+	
+	// On idle methods for the combat units.
+	// What to do when doing nothing.
 	void OnIdleMarine(const Unit* unit);
 	void OnIdleReaper(const Unit* unit);
+	void OnIdleMarauder(const Unit* unit);
+	void OnIdleHellion(const Unit* unit);
+	void OnIdleSeigeTank(const Unit* unit);
 	
+	// Utility for morhping units
+	void HellionMorph(const Units units, bool attacking, Point2D to);
+	void SiegeTankMorph(const Units units, bool attacking, Point2D to);
+	
+	Point2D halfway = Point2D(0, 0);
+
 private:
 	size_t numberIdleMarines = 0;
 };
